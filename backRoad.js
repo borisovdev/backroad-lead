8ce5b5c409f4fc3522143b61d2364db8e0a4dfaa
Alexander Borisov <burninghills@yandex.ru>
2020-05-09 19:26:24 +0300

    beta 0.2.0

--- backRoad.js
+++ backRoad.js
@@ -16,7 +16,8 @@ export default class {
   constructor(title, desc) {
     this.devMode = true;
     this.cookieUtil = new CookieUtil();
-    document.addEventListener('mouseleave', cn => this.isOutDocument(cn) )
+    document.addEventListener('mouseleave', cn => this.isOutDocument(cn) );
+    this.modalScripts();
     this.title = title;
     this.desc = desc;
   }
@@ -101,30 +102,17 @@ export default class {
   }
 
   modalScripts() {
-    return `
-      <script>
-          (function() {
-            const close = document.querySelector(".backroad__close");
-            if (close !== null) {
-              close.addEventListener("click", function(evt) {
-                  console.log(evt.target);
-                  console.log(this);
-                  this.parentNode.parentNode.display = "none"; 
-              });  
-            } else {
-                console.log(close);
-                return null;
-            }              
-          })();         
-      </script>
-    `
+    return window.closeBackRoadLead = function(el) {
+      console.log(el);
+      el.parentNode.parentNode.style.display = "none";
+    }
   }
 
   modalHTML() {
     return `
       <div id="backroad" class="backroad__overlay">
         <div class="backroad__content">
-          <div class="backroad__close"><svg height="329pt" viewBox="0 0 329.26933 329" width="329pt" xmlns="http://www.w3.org/2000/svg"><path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0"/></svg></div>
+          <div class="backroad__close" onclick="closeBackRoadLead(this);"><svg height="329pt" viewBox="0 0 329.26933 329" width="329pt" xmlns="http://www.w3.org/2000/svg"><path d="m194.800781 164.769531 128.210938-128.214843c8.34375-8.339844 8.34375-21.824219 0-30.164063-8.339844-8.339844-21.824219-8.339844-30.164063 0l-128.214844 128.214844-128.210937-128.214844c-8.34375-8.339844-21.824219-8.339844-30.164063 0-8.34375 8.339844-8.34375 21.824219 0 30.164063l128.210938 128.214843-128.210938 128.214844c-8.34375 8.339844-8.34375 21.824219 0 30.164063 4.15625 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921875-2.089844 15.082031-6.25l128.210937-128.214844 128.214844 128.214844c4.160156 4.160156 9.621094 6.25 15.082032 6.25 5.460937 0 10.921874-2.089844 15.082031-6.25 8.34375-8.339844 8.34375-21.824219 0-30.164063zm0 0"/></svg></div>
           <span class="backroad__content-title">${this.title}</span>
           <p class="backroad__content-desc">${this.desc}</p>
         </div>
@@ -148,7 +136,6 @@ export default class {
 
   openModal() {
     document.head.insertAdjacentHTML("beforeend", this.modalStyles());
-    document.head.insertAdjacentHTML("beforeend", this.modalScripts());
     document.body.insertAdjacentHTML("beforeend", this.modalHTML());
   }
 
---
+++ utils/closeBackRoadLead.js
@@ -0,0 +1,4 @@
+function closeBackRoadLead(el) {
+    console.log(el);
+    el.parentNode.parentNode.style.display = "none";
+}
